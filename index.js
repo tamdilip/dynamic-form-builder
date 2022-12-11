cda0dc957c7a04f5096d3a8e8e57cdeda0afb27b
Dilipan Somasundaram <tamdilip@gmail.com>
2022-12-11 22:38:37 +0530

    Dynamic env port added for container deployments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Datastore = require('nedb');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
